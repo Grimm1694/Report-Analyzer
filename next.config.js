d8838ecfccd38783bc58b73db005a83e9b0f3ba7
Sharan Reddy M <112468631+Grimm1694@users.noreply.github.com>
2025-06-20 06:16:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during production builds
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.externals = {
